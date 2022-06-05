ed138d731440bb83ad43a26675101439ba671bc8
williamcostaibvn <william.assessoria18@gmail.com>
2022-06-05 01:15:46 -0300

    Alterando Exercício de Python - Fibonatti While, para rodar em loopping

--- fibonacciWhile.py
+++ fibonacciWhile.py
@@ -1,12 +1,22 @@
-sequencia = int(input("Qual numero de Fibonacci deseja encontrar? "))
-                      
+#Declarando variaveis
+sequencia = 1
 ultimo = 0
 penultimo = 1
 n = 0
 
-while(n < sequencia):
-    print(n + 1,") ",ultimo)
-    proximo = ultimo + penultimo
-    ultimo = penultimo
-    penultimo = proximo
-    n += 1
+#Looping para encerrar o app quando for digitado "0"
+while(sequencia != 0):
+	sequencia = int(input("\nQual número de fibonacci deseja encontrar? "))
+
+	#Gerador em looping de sequencia Fibonatti com comando "while"
+	while(n < sequencia):
+	    print(n + 1,") ",ultimo)
+	    proximo = ultimo + penultimo
+	    ultimo = penultimo
+	    penultimo = proximo
+	    n += 1
+	ultimo = 0
+	penultimo = 1
+	n = 0
+
+#Fim
