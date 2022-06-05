5f47c2ee23b06ac38fe61801794154757636cb6a
williamcostaibvn <william.assessoria18@gmail.com>
2022-06-05 01:14:47 -0300

    Alterando Exercício de Python - Fibonatti For, para rodar em loopping

--- fibonacciFor.py
+++ fibonacciFor.py
@@ -1,12 +1,22 @@
-sequencia = int(input("Qual numero de fibonacci deseja encontrar? "))
-
+#Declarando variaveis
 penultimo = 0
 ultimo = 1
+sequencia = 1
 i = 1
 
-for n in range(0, sequencia):
-    print(i,") ", penultimo)
-    proximo = penultimo + ultimo
-    penultimo = ultimo
-    ultimo = proximo
-    i += 1
+#Looping para encerrar o app quando for digitado "0"
+while (sequencia != 0):
+    sequencia = int(input("\nQual número de fibonacci deseja encontrar? "))
+
+    #Gerador em looping de sequencia Fibonatti com comando "for"
+    for n in range(0, sequencia):
+        print(i,") ", penultimo)
+        proximo = penultimo + ultimo
+        penultimo = ultimo
+        ultimo = proximo
+        i += 1
+    penultimo = 0
+    ultimo = 1
+    i = 1
+
+#Fim
