e940c68c216cf3dfbc60bd5bae9633587b3b56bc
williamcostaibvn <william.assessoria18@gmail.com>
2022-06-26 20:35:28 -0300

    Alterando Exercício Python - Quitanda (Rafael) - Adicionando moedas como opção de troco

--- classeQuitanda.py
+++ classeQuitanda.py
@@ -6,20 +6,29 @@ class Quitanda:
         self.total = quantidade * valor
 
     def exibirPedido(self) -> None:
-        print('%s            %s               %7.2f               %7.2f' % (self.compra, self.qtd, self.preco, self.total))
+        print('%s            %s               %4.2f               %4.2f' % (self.compra, self.qtd, self.preco, self.total))
 
     def calculaTroco(self, divida, valorPago):
-        tipoCedulas = [int(20), int(5), int(1)]
-        print('\nDivida:     R$ ', divida)
-        print('Valor pago: R$ ', valorPago)
+        tipoCedulas = [20, 10, 5, 2]
+        tipoMoedas = [1, 0.5, 0.25, 0.1, 0.05]
+        print('\nDivida:     R$ %4.2f' % (divida))
+        print('Valor pago: R$ %4.2f' % (valorPago))
         troco = valorPago - divida
-        print('Troco:      R$ ', troco)
+        print('Troco:      R$ %3.2f' % (troco))
         print('')
         if(troco > 0):
             print('Devolva:')
             for cedula in tipoCedulas:
                 if troco >= cedula:
                     quantCedula = troco/cedula
-                    print('%s nota(s) de R$ %s.00' % (int(quantCedula), cedula))
+                    print('%s nota(s) de R$ %3.2f' % (int(quantCedula), cedula))
                     resto = troco%cedula
                     troco = resto
+            for moeda in tipoMoedas:
+                if troco >= moeda:
+                    quantMoeda = troco/moeda
+                    print('%s moeda(s) de R$ %3.2f' % (int(quantMoeda), moeda))
+                    resto = troco%moeda
+                    troco = resto
+        if troco < 0.05:
+            print('\nAinda falta devolver R$ %1.2f. Aceita uma Bala?' % (troco))
--- quitanda.py
+++ quitanda.py
@@ -1,15 +1,21 @@
 from classeQuitanda import Quitanda
 
 frutas = {
-    1 : 'morango',
-    2 : 'melancia', 
-    3 : 'laranja'
+    1 : 'Morango  ',
+    2 : 'Melancia ', 
+    3 : 'Laranja  ',
+    4 : 'Tanjerina',
+    5 : 'Caqui    ',
+    6 : 'Damasco  '
     }
 
 precoFrutas = {
-    1 : 1.00,
-    2 : 20.00, 
-    3 : 3.00
+    1 : 1.79,
+    2 : 19.99, 
+    3 : 2.99,
+    4 : 2.39,
+    5 : 4.49,
+    6 : 3.99
     }
 
 comprarMais = 's'
@@ -24,15 +30,19 @@ print()
 print('Preço bom | Qualidade | Bom Atendimento')
 
 # Laço do pedido de frutas
-while (comprarMais != 'n'):
+while (comprarMais != 'N'):
+
+    # Exibindo as Códigos, Frutas e Preços
     print('\n____________________________________________________')
     print('Essas são nossas frutas disponíveis hoje:\n')
     for fruit in frutas:
         print('{} - {} (R$ {}/Unid)'.format(fruit, frutas[fruit], (precoFrutas[fruit])))
     print('____________________________________________________')
+
+    # Recebendo e tratando o código da fruta que o cliente solicitou
     while True:
         try:
-            venda = int(input('\nInforme o número da fruta que você deseja comprar: '))
+            venda = int(input('\nInforme o Código da fruta que você deseja comprar: '))
             print('')
             if not 1 <= venda <= len(frutas):
                 raise ValueError()
@@ -41,16 +51,20 @@ while (comprarMais != 'n'):
         else:
             break
     
+    # Exibindo o preço da fruta selecionada
     preco = precoFrutas[venda]
-    print('\nO preço unitário do(a) {} é {} '.format(frutas[venda], preco))
+    print('\nCusta R$ {} cada unidade do(a) {} '.format(preco, frutas[venda]))
 
-    qtd = int(input('\nQuantas unidades de {} você gostaria? '.format(frutas[venda])))
+    # Solicitando a quantidade desejada da Fruta solicitada
+    qtd = int(input('\nQuantas unidades de você gostaria? '))
     print()
     
+    # Calculando valor de cada item e do total do pedido 
     total = qtd * preco
 
     totalPedido = totalPedido + total
 
+    # Anotando pedido na classeQuitanda.py
     pedido[frutas[venda]] = Quitanda(frutas[venda], qtd, preco)
 
     # Exibir pedido para o cliente
@@ -62,14 +76,16 @@ while (comprarMais != 'n'):
         pedido[compra].exibirPedido()
 
     print('----------------------------------------------------------------------')
-    print('Total de Itens: {} - Valor total da Compra: R$ {}'.format(len(pedido.keys()),totalPedido))
+    print('Total de Itens: {} - Valor total da Compra: R$ %4.2f'.format(int(len(pedido.keys()))) % (totalPedido))
 
+    # Encerrar laço do pedido de frutas - Sim/Não
     comprarMais = input('\nDeseja comprar mais alguma fruta (s/n)? ')
+    comprarMais = comprarMais.upper()
 
 # Recebendo pagamento
 pagamento = int(input('\nQual o valor do pagamento: '))
 
-# Teste para pagamento menor que valor do pedido
+# Teste para pagamento menor que valor do pedido, calculo de troco e quantidade de notas e moedas para devolver
 if(pagamento < totalPedido):
     print('O valor pago é menor que o valor da compra!')
     pagamento = int(input('Reveja o valor do pagamento: '))
@@ -79,6 +95,8 @@ else:
     pedido[frutas[venda]].calculaTroco(totalPedido, pagamento)
 
 # Finalizando programa
+print()
+print('----------------------------------------------------------------------')
 print()
 print('Compra finalizada, a Quitando do Joaquim agradece... Volte sempre!')
 print()
